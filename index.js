4f74043e3dbd3bb384a09f81290d4ab40c547e76
Jorge Bucaran <jbucaran@me.com>
2015-08-19 16:41:38 +0900

    add break to split text by columns

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var ansi = require("ansi-styles")
 ansi.line = { open: "\n", close: "" }
 ansi.tab = { open: "\t", close: "" }
+ansi._ = { open: " ", close: "" }
 
 module.exports = (function clor (s) {
   function $ (str) {
@@ -12,12 +13,26 @@ module.exports = (function clor (s) {
   $.toString = function () { return s + ansi.reset.close }
 
   $.log = function (logger) {
-    var args = [].slice.call(arguments, 0)
     if (typeof logger === "function") {
-      logger.apply(s, args.slice(1))
+      logger.apply(s, [].slice.call(arguments, 0).slice(1))
     } else {
-      console.log.apply(console, [s].concat(args))
+      console.log.apply(console, [s].concat([].slice.call(arguments, 0)))
     }
+  }
+
+  $.break = function (columns) {
+    var br = "", open = false
+    for (var i = 0, visible = 0; i < s.length; i++) {
+      br += s[i]
+      if (visible >= columns - 2 && s[i] === " ") {
+        br += "\n"
+        visible = 0
+      }
+      if (s[i] === "\u001b") open = true
+      if (!open) visible++
+      if (open && s[i] === "m") open = false
+    }
+    console.log(br)
   }
 
   Object.defineProperty($, "string", {
