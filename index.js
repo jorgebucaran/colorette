73dcd28bf114312ec74ce94eaaf9b1c21df0d8ff
Jorge Bucaran <mail@jorgebucaran.com>
2017-01-07 05:42:00 +0900

    Rewrite in more functional style. Remove logger. Add nested styles.

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-"use strict"
-var ansi = require("ansi-styles")
-ansi.line = { open: "\n", close: "" }
-ansi.tab = { open: "\t", close: "" }
+const ansi = {
+	black: ["\x1b[30m", "\x1b[39m"],
+	red: ["\x1b[31m", "\x1b[39m"],
+	green: ["\x1b[32m", "\x1b[39m"],
+	yellow: ["\x1b[33m", "\x1b[39m"],
+	blue: ["\x1b[34m", "\x1b[39m"],
+	magenta: ["\x1b[35m", "\x1b[39m"],
+	cyan: ["\x1b[36m", "\x1b[39m"],
+	white: ["\x1b[37m", "\x1b[39m"],
+	gray: ["\x1B[90m", "\x1b[39m"],
 
-module.exports = (function clor (s, open) {
-  function $ (str) {
-    return (str === undefined)
-      ? $.toString()
-      : clor(s + str + ansi.reset.close)
-  }
+	bgBlack: ["\x1b[40m", "\x1b[49m"],
+	bgRed: ["\x1b[41m", "\x1b[49m"],
+	bgGreen: ["\x1b[42m", "\x1b[49m"],
+	bgYellow: ["\x1b[43m", "\x1b[49m"],
+	bgBlue: ["\x1b[44m", "\x1b[49m"],
+	bgMagenta: ["\x1b[45m", "\x1b[49m"],
+	bgCyan: ["\x1b[46m", "\x1b[49m"],
+	bgWhite: ["\x1b[47m", "\x1b[49m"],
 
-  $.toString = function () {
-    return (open === true)
-      ? s + ansi.reset.close
-      : s
-  }
+	bold: ["\x1b[1m", "\x1b[22m"],
+	italic: ["\x1b[3m", "\x1b[23m"],
+	underline: ["\x1b[4m", "\x1b[24m"],
+	inverse: ["\x1b[7m", "\x1b[27m"],
+	strikethrough: ["\x1b[9m", "\x1b[29m"],
 
-  $.log = function (logger) {
-    var args = [].slice.call(arguments, 0)
-    if (typeof logger === "function") {
-      logger.apply(s, args.slice(1))
-    } else {
-      console.log.apply(console, [s].concat(args))
-    }
-  }
+    newLine: ["\n", ""],
+    tab: ["\t", ""],
+    reset: ["\x1b[0m"]
+}
 
-  Object.defineProperty($, "string", {
-    get: function () { return $.toString() }
-  })
+module.exports = (function clor(prev, close) {
+    const wrap = s => clor(wrap.toString(s))
 
-  Object.keys(ansi).forEach(function (style) {
-    Object.defineProperty($, style, {
-      get: function () {
-        return clor(s + ansi[style].open, true)
-      }
-    })
-  })
+    wrap.toString = s => prev + (s || "") + (close || ansi.reset[0])
 
-  return $
-}(""))
+    Object.keys(ansi).forEach(style =>
+        Object.defineProperty(wrap, style, {
+            get: _ => clor(prev + ansi[style][0], ansi[style][1])
+        })
+    )
+
+    return wrap
+} (""))
+
+
+
+
+
