03f4603791cf6d400a735d24933485866babdb03
Jorge Bucaran <jbucaran@gmail.com>
2017-01-09 16:16:36 +0900

    Simplify even more!

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
-const ansi = {
-	black: ["\x1b[30m", "\x1b[39m"],
-	red: ["\x1b[31m", "\x1b[39m"],
-	green: ["\x1b[32m", "\x1b[39m"],
-	yellow: ["\x1b[33m", "\x1b[39m"],
-	blue: ["\x1b[34m", "\x1b[39m"],
-	magenta: ["\x1b[35m", "\x1b[39m"],
-	cyan: ["\x1b[36m", "\x1b[39m"],
-	white: ["\x1b[37m", "\x1b[39m"],
-	gray: ["\x1B[90m", "\x1b[39m"],
+const ansi = require("./ansi")
 
-	bgBlack: ["\x1b[40m", "\x1b[49m"],
-	bgRed: ["\x1b[41m", "\x1b[49m"],
-	bgGreen: ["\x1b[42m", "\x1b[49m"],
-	bgYellow: ["\x1b[43m", "\x1b[49m"],
-	bgBlue: ["\x1b[44m", "\x1b[49m"],
-	bgMagenta: ["\x1b[45m", "\x1b[49m"],
-	bgCyan: ["\x1b[46m", "\x1b[49m"],
-	bgWhite: ["\x1b[47m", "\x1b[49m"],
+module.exports = (function Clor(prev, wrap = ansi.reset[0]) {
+	const clor = s => Clor(clor.toString(s))
 
-	dim: ["\x1b[2m", "\x1b[22m"],
-	bold: ["\x1b[1m", "\x1b[22m"],
-	hidden: ["\x1b[8m", "\x1b[28m"],
-	italic: ["\x1b[3m", "\x1b[23m"],
-	underline: ["\x1b[4m", "\x1b[24m"],
-	inverse: ["\x1b[7m", "\x1b[27m"],
-	strikethrough: ["\x1b[9m", "\x1b[29m"],
+	clor.toString = (s = "") => prev + s + wrap
 
-	newLine: ["\n", ""],
-	tab: ["\t", ""],
-	reset: ["\x1b[0m"]
-}
+	Object.keys(ansi).forEach(k => Object.defineProperty(clor, k, {
+		get: _ => Clor(prev + ansi[k][0], ansi[k][1])
+	}))
 
-module.exports = (function clor(carry, close) {
-	const wrap = s => clor(wrap.toString(s))
-
-	wrap.toString = s => carry + (s || "") + (close || ansi.reset[0])
-
-	Object.keys(ansi).forEach(style =>
-		Object.defineProperty(wrap, style, {
-			get: _ => clor(carry + ansi[style][0], ansi[style][1])
-		})
-	)
-
-	return wrap
+	return clor
 } (""))
+
