b1816a471eec76bb6f71d25892a405269a456009
Jorge Bucaran <mail@jorgebucaran.com>
2017-02-19 03:10:20 +0900

    Simplify the generation of ANSI styles.

--- ansi.js
+++ ansi.js
@@ -1,42 +1,41 @@
-const ansi = module.exports = {
-	black: ["\x1b[30m", "\x1b[39m"],
-	red: ["\x1b[31m", "\x1b[39m"],
-	green: ["\x1b[32m", "\x1b[39m"],
-	yellow: ["\x1b[33m", "\x1b[39m"],
-	blue: ["\x1b[34m", "\x1b[39m"],
-	magenta: ["\x1b[35m", "\x1b[39m"],
-	cyan: ["\x1b[36m", "\x1b[39m"],
-	white: ["\x1b[37m", "\x1b[39m"],
-	gray: ["\x1B[90m", "\x1b[39m"],
+const none = ["", ""]
 
-	bgBlack: ["\x1b[40m", "\x1b[49m"],
-	bgRed: ["\x1b[41m", "\x1b[49m"],
-	bgGreen: ["\x1b[42m", "\x1b[49m"],
-	bgYellow: ["\x1b[43m", "\x1b[49m"],
-	bgBlue: ["\x1b[44m", "\x1b[49m"],
-	bgMagenta: ["\x1b[45m", "\x1b[49m"],
-	bgCyan: ["\x1b[46m", "\x1b[49m"],
-	bgWhite: ["\x1b[47m", "\x1b[49m"],
-
-	dim: ["\x1b[2m", "\x1b[22m"],
-	bold: ["\x1b[1m", "\x1b[22m"],
-	hidden: ["\x1b[8m", "\x1b[28m"],
-	italic: ["\x1b[3m", "\x1b[23m"],
-	underline: ["\x1b[4m", "\x1b[24m"],
-	inverse: ["\x1b[7m", "\x1b[27m"],
-	strikethrough: ["\x1b[9m", "\x1b[29m"],
-
-	newLine: ["\n", "\x1b[0m"],
-	tab: ["\t", "\x1b[0m"],
-	reset: ["\x1b[0m"]
-}
-
-const hasColor = _ =>
+const support =
 	process.env.FORCE_COLOR ||
 	process.platform === "win32" ||
 	process.stdout.isTTY &&
 	process.env.TERM &&
 	process.env.TERM !== "dumb"
 
-Object.keys(ansi).forEach(k => ansi[k] = hasColor() ? ansi[k] : ["", ""])
+module.exports = {
+	black: support ? ["\x1b[30m", "\x1b[39m"] : none,
+	red: support ? ["\x1b[31m", "\x1b[39m"] : none,
+	green: support ? ["\x1b[32m", "\x1b[39m"] : none,
+	yellow: support ? ["\x1b[33m", "\x1b[39m"] : none,
+	blue: support ? ["\x1b[34m", "\x1b[39m"] : none,
+	magenta: support ? ["\x1b[35m", "\x1b[39m"] : none,
+	cyan: support ? ["\x1b[36m", "\x1b[39m"] : none,
+	white: support ? ["\x1b[37m", "\x1b[39m"] : none,
+	gray: support ? ["\x1B[90m", "\x1b[39m"] : none,
 
+	bgBlack: support ? ["\x1b[40m", "\x1b[49m"] : none,
+	bgRed: support ? ["\x1b[41m", "\x1b[49m"] : none,
+	bgGreen: support ? ["\x1b[42m", "\x1b[49m"] : none,
+	bgYellow: support ? ["\x1b[43m", "\x1b[49m"] : none,
+	bgBlue: support ? ["\x1b[44m", "\x1b[49m"] : none,
+	bgMagenta: support ? ["\x1b[45m", "\x1b[49m"] : none,
+	bgCyan: support ? ["\x1b[46m", "\x1b[49m"] : none,
+	bgWhite: support ? ["\x1b[47m", "\x1b[49m"] : none,
+
+	dim: support ? ["\x1b[2m", "\x1b[22m"] : none,
+	bold: support ? ["\x1b[1m", "\x1b[22m"] : none,
+	hidden: support ? ["\x1b[8m", "\x1b[28m"] : none,
+	italic: support ? ["\x1b[3m", "\x1b[23m"] : none,
+	underline: support ? ["\x1b[4m", "\x1b[24m"] : none,
+	inverse: support ? ["\x1b[7m", "\x1b[27m"] : none,
+	strikethrough: support ? ["\x1b[9m", "\x1b[29m"] : none,
+
+	newLine: ["\n", support ? "\x1b[0m" : ""],
+	tab: ["\t", support ? "\x1b[0m" : ""],
+	reset: support ? ["\x1b[0m"] : none
+}
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const ansi = require("./ansi")
 
-module.exports = (function Clor(prev, wrap) {
+module.exports = (function Clor(old, close) {
 	const clor = s => Clor(clor.toString(s))
 
-	clor.toString = s => prev + (s || "") + (wrap || ansi.reset[0])
+	clor.toString = s => old + (s || "") + (close || ansi.reset[0])
 
-	Object.keys(ansi).forEach(k => Object.defineProperty(clor, k, {
-		get: _ => Clor(prev + ansi[k][0], ansi[k][1])
+	Object.keys(ansi).forEach(name => Object.defineProperty(clor, name, {
+		get: () => Clor(old + ansi[name][0], ansi[name][1])
 	}))
 
 	return clor
-} (""))
+}(""))
