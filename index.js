1e4c0070080a270390c5612e211be2bc99a75e5f
Jorge Bucaran <jbucaran@gmail.com>
2017-06-16 02:19:57 +0900

    Use prettier to format code.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const ansi = require("./ansi")
 
 module.exports = (function Clor(old, close) {
-	const clor = s => Clor(clor.toString(s))
+  const clor = s => Clor(clor.toString(s))
 
-	clor.toString = s => old + (s || "") + (close || ansi.reset[0])
+  clor.toString = s => old + (s || "") + (close || ansi.reset[0])
 
-	Object.keys(ansi).forEach(name => Object.defineProperty(clor, name, {
-		get: () => Clor(old + ansi[name][0], ansi[name][1])
-	}))
+  Object.keys(ansi).forEach(name =>
+    Object.defineProperty(clor, name, {
+      get: () => Clor(old + ansi[name][0], ansi[name][1])
+    })
+  )
 
-	return clor
-}(""))
+  return clor
+})("")
