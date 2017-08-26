6a3d87fe055d44e99e0e8886ac28672bd944b708
Jorge Bucaran <jbucaran@gmail.com>
2017-08-26 23:00:46 +0900

    Remove c.js. Close #12.

--- ansi.js
+++ ansi.js
@@ -1,41 +1,39 @@
-const support =
-  process.env.FORCE_COLOR ||
-  process.platform === "win32" ||
-  (process.stdout.isTTY &&
-    process.env.TERM &&
-    process.env.TERM !== "dumb")
-
-const none = ansi => (support ? ansi : ["", ""])
-
-module.exports = {
-  reset: none(["\x1b[0m"]),
+module.exports = (strip => ({
+  reset: strip(["\x1b[0m"]),
   newLine: ["\n", ""],
   tab: ["\t", ""],
 
-  black: none(["\x1b[30m", "\x1b[39m"]),
-  red: none(["\x1b[31m", "\x1b[39m"]),
-  green: none(["\x1b[32m", "\x1b[39m"]),
-  yellow: none(["\x1b[33m", "\x1b[39m"]),
-  blue: none(["\x1b[34m", "\x1b[39m"]),
-  magenta: none(["\x1b[35m", "\x1b[39m"]),
-  cyan: none(["\x1b[36m", "\x1b[39m"]),
-  white: none(["\x1b[37m", "\x1b[39m"]),
-  gray: none(["\x1B[90m", "\x1b[39m"]),
+  black: strip(["\x1b[30m", "\x1b[39m"]),
+  red: strip(["\x1b[31m", "\x1b[39m"]),
+  green: strip(["\x1b[32m", "\x1b[39m"]),
+  yellow: strip(["\x1b[33m", "\x1b[39m"]),
+  blue: strip(["\x1b[34m", "\x1b[39m"]),
+  magenta: strip(["\x1b[35m", "\x1b[39m"]),
+  cyan: strip(["\x1b[36m", "\x1b[39m"]),
+  white: strip(["\x1b[37m", "\x1b[39m"]),
+  gray: strip(["\x1B[90m", "\x1b[39m"]),
 
-  bgBlack: none(["\x1b[40m", "\x1b[49m"]),
-  bgRed: none(["\x1b[41m", "\x1b[49m"]),
-  bgGreen: none(["\x1b[42m", "\x1b[49m"]),
-  bgYellow: none(["\x1b[43m", "\x1b[49m"]),
-  bgBlue: none(["\x1b[44m", "\x1b[49m"]),
-  bgMagenta: none(["\x1b[45m", "\x1b[49m"]),
-  bgCyan: none(["\x1b[46m", "\x1b[49m"]),
-  bgWhite: none(["\x1b[47m", "\x1b[49m"]),
+  bgBlack: strip(["\x1b[40m", "\x1b[49m"]),
+  bgRed: strip(["\x1b[41m", "\x1b[49m"]),
+  bgGreen: strip(["\x1b[42m", "\x1b[49m"]),
+  bgYellow: strip(["\x1b[43m", "\x1b[49m"]),
+  bgBlue: strip(["\x1b[44m", "\x1b[49m"]),
+  bgMagenta: strip(["\x1b[45m", "\x1b[49m"]),
+  bgCyan: strip(["\x1b[46m", "\x1b[49m"]),
+  bgWhite: strip(["\x1b[47m", "\x1b[49m"]),
 
-  dim: none(["\x1b[2m", "\x1b[22m"]),
-  bold: none(["\x1b[1m", "\x1b[22m"]),
-  hidden: none(["\x1b[8m", "\x1b[28m"]),
-  italic: none(["\x1b[3m", "\x1b[23m"]),
-  underline: none(["\x1b[4m", "\x1b[24m"]),
-  inverse: none(["\x1b[7m", "\x1b[27m"]),
-  strikethrough: none(["\x1b[9m", "\x1b[29m"])
-}
+  dim: strip(["\x1b[2m", "\x1b[22m"]),
+  bold: strip(["\x1b[1m", "\x1b[22m"]),
+  hidden: strip(["\x1b[8m", "\x1b[28m"]),
+  italic: strip(["\x1b[3m", "\x1b[23m"]),
+  underline: strip(["\x1b[4m", "\x1b[24m"]),
+  inverse: strip(["\x1b[7m", "\x1b[27m"]),
+  strikethrough: strip(["\x1b[9m", "\x1b[29m"])
+}))(
+  ansi =>
+    process.env.FORCE_COLOR ||
+    process.platform === "win32" ||
+    (process.stdout.isTTY && process.env.TERM && process.env.TERM !== "dumb")
+      ? ansi
+      : ["", ""]
+)
--- test.js
+++ test.js
@@ -1,40 +1,14 @@
 const clor = require("./")
 
-console.log("" +
-    clor("How").newLine("are").newLine("you?")
-)
-
-console.log(
-    `${clor.red.bold("How").newLine.inverse("are").newLine.italic("you?")}`
-)
-
-console.log(
-    clor.red.bold("How") + "\n" + clor.red.inverse("are") + "\n" + clor.red.italic("you?")
-)
-
-console.log("" +
-    clor.bold(clor.red("Bold red")).blue(" and blue")
-)
+console.log(`${clor.red("Beans")}`)
+console.log(`${clor.red.bold("What's").newLine.inverse("up?")}`)
+console.log(clor.red.bold("What's") + "\n" + clor.red.inverse("up?"))
+console.log(`${clor.bold(clor.red("Bold & Red"))}`)
 
 const Styles = {
-    em: s => clor.bold.inverse(s)
+  em(s) {
+    return clor.bgBlack.yellow(s)
+  }
 }
 
-console.log("" +
-    clor(Styles.em("Known trope")).bold(" or meme")
-)
-
-// Tagged Literals
-
-const c = require("./c")
-const x = "Bold", y = "red", z = "blue"
-
-console.log(
-    c`<bold>${x}, <red>${y}</red></bold> and <blue>${z}.</blue>`
-)
-
-const name = ["Leia", "Han", "Ben"]
-    [Math.floor(Math.random() * 4)]
-
-console.log(
-    c`Hi ${name ? c`<bold>${name}</bold>` : "everyone"}.`)
+console.log(`${clor(Styles.em("Known trope")).bold(" or meme")}`)
