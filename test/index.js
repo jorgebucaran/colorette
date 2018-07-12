53c1238fc20c3e3bad2565dacb95d1c795848ef1
Jorge Bucaran <mail@jorgebucaran.com>
2018-07-12 11:37:43 +0900

    Rename left/right to open/close to better compat with chalk/ansi-styles.

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const test = require("tape")
 test("styles", t => {
   Object.keys(STYLES).map(style => {
     const actual = x[style](style)
-    const expected = `${STYLES[style].left}${style}${STYLES[style].right}`
+    const expected = `${STYLES[style].open}${style}${STYLES[style].close}`
 
     t.is(actual, expected, actual)
   })
@@ -15,9 +15,9 @@ test("chains", t => {
   const { red, bold, underline, italic } = STYLES
   const fixture = "Red, bold, underline and italic."
   const actual = x.red.bold.underline.italic(fixture)
-  const expected = `${italic.left}${underline.left}${bold.left}${
-    red.left
-  }${fixture}${red.right}${bold.right}${underline.right}${italic.right}`
+  const expected = `${italic.open}${underline.open}${bold.open}${
+    red.open
+  }${fixture}${red.close}${bold.close}${underline.close}${italic.close}`
 
   t.is(actual, expected, actual)
   t.end()
@@ -29,9 +29,9 @@ test("nested", t => {
   const actual = x.red(
     `Red ${x.blue.bold("Blue Bold")} Red ${x.green("Green")} Red`
   )
-  const expected = `${red.left}Red ${bold.left}${blue.left}Blue Bold${
-    red.left
-  }${bold.right} Red ${green.left}Green${red.left} Red${red.right}`
+  const expected = `${red.open}Red ${bold.open}${blue.open}Blue Bold${
+    red.open
+  }${bold.close} Red ${green.open}Green${red.open} Red${red.close}`
 
   t.is(actual, expected, actual)
   t.end()
