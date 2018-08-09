17d45d353ac53c184cb44e55bc8195395af4a30f
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-09 22:32:41 +0900

    upgrade: swap tape for testmatrix

--- test/index.js
+++ test/index.js
@@ -1,89 +1,87 @@
 const tc = require("..")
-const test = require("tape")
+const red = tc.Styles.red
+const blue = tc.Styles.blue
+const cyan = tc.Styles.cyan
+const bold = tc.Styles.bold
+const italic = tc.Styles.italic
+const inverse = tc.Styles.inverse
+const underline = tc.Styles.underline
 
-test("styles", t => {
-  Object.keys(tc.Styles).map(k => {
-    const actual = tc[k](k)
-    const expected = `${tc.Styles[k].open}${k}${tc.Styles[k].close}`
+const equal = require("testmatrix").equal
 
-    t.is(actual, expected, actual)
-  })
-  t.end()
+const Equal = (actual, expected) => ({
+  name: actual,
+  assert: equal,
+  actual,
+  expected
 })
 
-test("numbers", t => {
-  const actual = tc.inverse.bgYellow(Math.PI)
-  const expected =
-    tc.Styles.inverse.open +
-    tc.Styles.bgYellow.open +
-    Math.PI +
-    tc.Styles.bgYellow.close +
-    tc.Styles.inverse.close
-
-  t.is(actual, expected, actual)
-
-  t.end()
-})
-
-test("chains", t => {
-  const red = tc.Styles.red
-  const bold = tc.Styles.bold
-  const italic = tc.Styles.italic
-  const inverse = tc.Styles.inverse
-  const underline = tc.Styles.underline
-
-  const fixture = "Red, bold, italic, inverse & underline."
-  const actual = tc.red.bold.italic.inverse.underline(fixture)
-  const expected =
-    red.open +
-    bold.open +
-    italic.open +
-    inverse.open +
-    underline.open +
-    fixture +
-    underline.close +
-    inverse.close +
-    italic.close +
-    bold.close +
-    red.close
-
-  t.is(actual, expected, actual)
-  t.end()
-})
-
-test("nested", t => {
-  const red = tc.Styles.red
-  const blue = tc.Styles.blue
-  const cyan = tc.Styles.cyan
-  const bold = tc.Styles.bold
-  const italic = tc.Styles.italic
-  const inverse = tc.Styles.inverse
-  const underline = tc.Styles.underline
-
-  const actual = tc.red(
-    `Red ${tc.inverse("Inverse Red")} ${tc.blue.bold(
-      `Blue ${tc.italic("Italic")} Bold`
-    )} Red ${tc.cyan.inverse(
-      `Inverse ${tc.underline("Underline")} Cyan`
-    )} ${tc.italic("Italic")} Red`
+const Basic = style =>
+  Equal(
+    tc[style](style),
+    tc.Styles[style].open + style + tc.Styles[style].close
   )
 
-  const expected = `${red.open}Red ${inverse.open}Inverse Red${inverse.close} ${
-    blue.open
-  }${bold.open}Blue ${italic.open}Italic${italic.close} Bold${bold.close}${
-    red.open
-  } Red ${cyan.open}${inverse.open}Inverse ${underline.open}Underline${
-    underline.close
-  } Cyan${inverse.close}${red.open} ${italic.open}Italic${italic.close} Red${
-    red.close
-  }`
+const Chain = (style, rightChain) => {
+  const chain = [style].concat(rightChain)
+  const name = chain.join(" ")
 
-  t.is(actual, expected, actual)
-  t.end()
-})
+  return Equal(
+    chain.reduce((getter, i) => getter[i], tc)(name),
+    chain.reduceRight(
+      (name, i) =>
+        (tc.Styles[i].open + name).replace(
+          tc.Styles[i].old,
+          tc.Styles[i].open
+        ) + tc.Styles[i].close,
+      name
+    )
+  )
+}
 
-test("toggle", t => {
-  tc.enabled = false
-  t.is(tc.red.bold.inverse.dim.underline("Ok"), "Ok")
-  t.end()
-})
+exports.default = {
+  styles: Object.keys(tc.Styles).map(Basic),
+  chaining: Object.keys(tc.Styles).map(k =>
+    Chain(k, ["bold", "italic", "underline"])
+  ),
+  others: [new Date(), -1e10, -1, -0.1, 0, 0.1, 1, 1e10].map(n =>
+    Equal(
+      tc.inverse.bgYellow(n),
+      tc.Styles.inverse.open +
+        tc.Styles.bgYellow.open +
+        n +
+        tc.Styles.bgYellow.close +
+        tc.Styles.inverse.close
+    )
+  ),
+  nesting: [
+    Equal(
+      tc.red(
+        `Red ${tc.inverse("Inverse Red")} ${tc.blue.bold(
+          `Blue ${tc.italic("Italic")} Bold`
+        )} Red ${tc.cyan.inverse(
+          `Inverse ${tc.underline("Underline")} Cyan`
+        )} ${tc.italic("Italic")} Red`
+      ),
+      `${red.open}Red ${inverse.open}Inverse Red${inverse.close} ${blue.open}${
+        bold.open
+      }Blue ${italic.open}Italic${italic.close} Bold${bold.close}${
+        red.open
+      } Red ${cyan.open}${inverse.open}Inverse ${underline.open}Underline${
+        underline.close
+      } Cyan${inverse.close}${red.open} ${italic.open}Italic${
+        italic.close
+      } Red${red.close}`
+    )
+  ],
+  "disable color support": [
+    {
+      assert: equal,
+      actual: done => {
+        tc.enabled = false
+        done(tc.red.bold.inverse.dim.underline("No Color"), (tc.enabled = true))
+      },
+      expected: "No Color"
+    }
+  ]
+}
