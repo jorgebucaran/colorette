44ec1d2eb60e75ffaf5d82bfed346bc18218111a
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-12 17:43:49 +0900

    refactor: rename test constructors
    Append ..Test to every test constructor. At this time it seems like
    a reasonable naming guideline, but I am still unsure.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,4 @@
 const tc = require("..")
-const red = tc.Styles.red
-const blue = tc.Styles.blue
-const cyan = tc.Styles.cyan
-const bold = tc.Styles.bold
-const italic = tc.Styles.italic
-const inverse = tc.Styles.inverse
-const underline = tc.Styles.underline
-
 const equal = require("testmatrix").equal
 
 const Equal = (actual, expected) => ({
@@ -16,13 +8,13 @@ const Equal = (actual, expected) => ({
   expected
 })
 
-const Basic = style =>
+const StyleTest = style =>
   Equal(
     tc[style](style),
     tc.Styles[style].open + style + tc.Styles[style].close
   )
 
-const Chain = (style, rightChain) => {
+const ChainTest = rightChain => style => {
   const chain = [style].concat(rightChain)
   const name = chain.join(" ")
 
@@ -40,11 +32,32 @@ const Chain = (style, rightChain) => {
 }
 
 exports.default = {
-  styles: Object.keys(tc.Styles).map(Basic),
-  chaining: Object.keys(tc.Styles).map(k =>
-    Chain(k, ["bold", "italic", "underline"])
+  "using styles": Object.keys(tc.Styles).map(StyleTest),
+  "chaining styles": Object.keys(tc.Styles).map(
+    ChainTest(["bold", "italic", "underline"])
   ),
-  others: [new Date(), -1e10, -1, -0.1, 0, 0.1, 1, 1e10].map(n =>
+  "nesting styles": [tc.Styles].map(
+    ({ red, blue, cyan, bold, italic, inverse, underline }) =>
+      Equal(
+        tc.red(
+          `Red ${tc.inverse("Inverse Red")} ${tc.blue.bold(
+            `Blue ${tc.italic("Italic")} Bold`
+          )} Red ${tc.cyan.inverse(
+            `Inverse ${tc.underline("Underline")} Cyan`
+          )} ${tc.italic("Italic")} Red`
+        ),
+        `${red.open}Red ${inverse.open}Inverse Red${inverse.close} ${
+          blue.open
+        }${bold.open}Blue ${italic.open}Italic${italic.close} Bold${
+          bold.close
+        }${red.open} Red ${cyan.open}${inverse.open}Inverse ${
+          underline.open
+        }Underline${underline.close} Cyan${inverse.close}${red.open} ${
+          italic.open
+        }Italic${italic.close} Red${red.close}`
+      )
+  ),
+  "numbers & others": [new Date(), -1e10, -1, -0.1, 0, 0.1, 1, 1e10].map(n =>
     Equal(
       tc.inverse.bgYellow(n),
       tc.Styles.inverse.open +
@@ -54,26 +67,6 @@ exports.default = {
         tc.Styles.inverse.close
     )
   ),
-  nesting: [
-    Equal(
-      tc.red(
-        `Red ${tc.inverse("Inverse Red")} ${tc.blue.bold(
-          `Blue ${tc.italic("Italic")} Bold`
-        )} Red ${tc.cyan.inverse(
-          `Inverse ${tc.underline("Underline")} Cyan`
-        )} ${tc.italic("Italic")} Red`
-      ),
-      `${red.open}Red ${inverse.open}Inverse Red${inverse.close} ${blue.open}${
-        bold.open
-      }Blue ${italic.open}Italic${italic.close} Bold${bold.close}${
-        red.open
-      } Red ${cyan.open}${inverse.open}Inverse ${underline.open}Underline${
-        underline.close
-      } Cyan${inverse.close}${red.open} ${italic.open}Italic${
-        italic.close
-      } Red${red.close}`
-    )
-  ],
   "disable color support": [
     {
       assert: equal,
