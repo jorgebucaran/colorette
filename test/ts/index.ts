5e59ab26684311eb42fb06bc493102e67ae4350b
Jorge Bucaran <mail@jorgebucaran.com>
2020-05-17 03:16:09 +0900

    Remove crappy ts smoke tests

--- test/ts/index.ts
+++
@@ -1,9 +0,0 @@
-import { red, green, bold, options, Style } from "colorette"
-
-options.enabled = true
-
-console.log(`
-  Beets are ${red("red")},
-  Cucumbers are ${green("green")},
-  ${bold("Colorette!")}.
-`)
