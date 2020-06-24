a3ec3fbbd0fb51115a97a8eb6fdf0ac168283014
Andrey Sitnik <andrey@sitnik.ru>
2020-06-24 15:34:25 -0400

    Add ESM support

--- index.js
+++ index.js
@@ -27,50 +27,93 @@ const init = (open, close) => {
   )
 }
 
+const options = Object.defineProperty({}, "enabled", {
+  get: () => enabled,
+  set: (value) => (enabled = value),
+})
+const reset = init(0, 0)
+const bold = raw("\x1b[1m", "\x1b[22m", /\x1b\[22m/g, "\x1b[22m\x1b[1m")
+const dim = raw("\x1b[2m", "\x1b[22m", /\x1b\[22m/g, "\x1b[22m\x1b[2m")
+const italic = init(3, 23)
+const underline = init(4, 24)
+const inverse = init(7, 27)
+const hidden = init(8, 28)
+const strikethrough = init(9, 29)
+const black = init(30, 39)
+const red = init(31, 39)
+const green = init(32, 39)
+const yellow = init(33, 39)
+const blue = init(34, 39)
+const magenta = init(35, 39)
+const cyan = init(36, 39)
+const white = init(37, 39)
+const gray = init(90, 39)
+const bgBlack = init(40, 49)
+const bgRed = init(41, 49)
+const bgGreen = init(42, 49)
+const bgYellow = init(43, 49)
+const bgBlue = init(44, 49)
+const bgMagenta = init(45, 49)
+const bgCyan = init(46, 49)
+const bgWhite = init(47, 49)
+const blackBright = init(90, 39)
+const redBright = init(91, 39)
+const greenBright = init(92, 39)
+const yellowBright = init(93, 39)
+const blueBright = init(94, 39)
+const magentaBright = init(95, 39)
+const cyanBright = init(96, 39)
+const whiteBright = init(97, 39)
+const bgBlackBright = init(100, 49)
+const bgRedBright = init(101, 49)
+const bgGreenBright = init(102, 49)
+const bgYellowBright = init(103, 49)
+const bgBlueBright = init(104, 49)
+const bgMagentaBright = init(105, 49)
+const bgCyanBright = init(106, 49)
+const bgWhiteBright = init(107, 49)
+
 module.exports = {
-  options: Object.defineProperty({}, "enabled", {
-    get: () => enabled,
-    set: (value) => (enabled = value),
-  }),
-  reset: init(0, 0),
-  bold: raw("\x1b[1m", "\x1b[22m", /\x1b\[22m/g, "\x1b[22m\x1b[1m"),
-  dim: raw("\x1b[2m", "\x1b[22m", /\x1b\[22m/g, "\x1b[22m\x1b[2m"),
-  italic: init(3, 23),
-  underline: init(4, 24),
-  inverse: init(7, 27),
-  hidden: init(8, 28),
-  strikethrough: init(9, 29),
-  black: init(30, 39),
-  red: init(31, 39),
-  green: init(32, 39),
-  yellow: init(33, 39),
-  blue: init(34, 39),
-  magenta: init(35, 39),
-  cyan: init(36, 39),
-  white: init(37, 39),
-  gray: init(90, 39),
-  bgBlack: init(40, 49),
-  bgRed: init(41, 49),
-  bgGreen: init(42, 49),
-  bgYellow: init(43, 49),
-  bgBlue: init(44, 49),
-  bgMagenta: init(45, 49),
-  bgCyan: init(46, 49),
-  bgWhite: init(47, 49),
-  blackBright: init(90, 39),
-  redBright: init(91, 39),
-  greenBright: init(92, 39),
-  yellowBright: init(93, 39),
-  blueBright: init(94, 39),
-  magentaBright: init(95, 39),
-  cyanBright: init(96, 39),
-  whiteBright: init(97, 39),
-  bgBlackBright: init(100, 49),
-  bgRedBright: init(101, 49),
-  bgGreenBright: init(102, 49),
-  bgYellowBright: init(103, 49),
-  bgBlueBright: init(104, 49),
-  bgMagentaBright: init(105, 49),
-  bgCyanBright: init(106, 49),
-  bgWhiteBright: init(107, 49),
+  options,
+  reset,
+  bold,
+  dim,
+  italic,
+  underline,
+  inverse,
+  hidden,
+  strikethrough,
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  gray,
+  bgBlack,
+  bgRed,
+  bgGreen,
+  bgYellow,
+  bgBlue,
+  bgMagenta,
+  bgCyan,
+  bgWhite,
+  blackBright,
+  redBright,
+  greenBright,
+  yellowBright,
+  blueBright,
+  magentaBright,
+  cyanBright,
+  whiteBright,
+  bgBlackBright,
+  bgRedBright,
+  bgGreenBright,
+  bgYellowBright,
+  bgBlueBright,
+  bgMagentaBright,
+  bgCyanBright,
+  bgWhiteBright,
 }
