c38e105abd77aca2ecb5cabc760943e803aa7f84
Jorge Bucaran <mail@jorgebucaran.com>
2021-10-03 00:45:22 +0900

    Improve benchmark readability

--- bench/index.js
+++ bench/index.js
@@ -1,34 +1,21 @@
-import bench from "benchmark"
 import * as colorette from "../index.js"
-import ansiColors from "ansi-colors"
-import colors from "colors"
+
+import bench from "benchmark"
 import chalk from "chalk"
 import kleur from "kleur"
+import colors from "colors"
+import ansicolors from "ansi-colors"
 
-const runBenchmark = (test, modules) =>
-  Object.keys(modules)
-    .reduce(
-      (bench, id) => bench.add(id, test.bind({}, modules[id])),
-      new bench.Suite().on("cycle", ({ target: { name, hz } }) =>
-        console.log(`${name} × ${Math.floor(hz).toLocaleString()} ops/sec`)
-      )
-    )
-    .run()
+const test = (c) =>
+  c.red(`${c.bold(`${c.cyan(`${c.yellow("yellow")}cyan`)}`)}red`)
 
-runBenchmark(
-  (c) =>
-    c.red(
-      `${c.blue(
-        `${c.bold(`${c.yellow("foo")}${c.underline("foo")}`)}${c.magenta(
-          `${c.white("foo")}${c.cyan("foo")}`
-        )}`
-      )}`
-    ),
-  {
-    colorette,
-    chalk,
-    kleur,
-    colors,
-    "ansi-colors": ansiColors,
-  }
-)
+new bench.Suite()
+  .add("chalk", () => test(chalk))
+  .add("kleur", () => test(kleur))
+  .add("colors", () => test(colors))
+  .add("ansi-colors", () => test(ansicolors))
+  .add("colorette", () => test(colorette))
+  .on("cycle", ({ target: { name, hz } }) =>
+    console.log(`${name} × ${Math.floor(hz).toLocaleString()} op/s`)
+  )
+  .run()
