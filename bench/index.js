71be83ac8a909a7571e2e4c6aa56a8ec6481eced
Jorge Bucaran <mail@jorgebucaran.com>
2021-10-03 13:00:37 +0900

    Add picocolors to benchmark

--- bench/index.js
+++ bench/index.js
@@ -4,6 +4,7 @@ import bench from "benchmark"
 import chalk from "chalk"
 import kleur from "kleur"
 import colors from "colors"
+import picocolors from "picocolors"
 import ansicolors from "ansi-colors"
 
 const test = (c) =>
@@ -14,6 +15,7 @@ new bench.Suite()
   .add("kleur", () => test(kleur))
   .add("colors", () => test(colors))
   .add("ansi-colors", () => test(ansicolors))
+  .add("picocolors", () => test(picocolors))
   .add("colorette", () => test(colorette))
   .on("cycle", ({ target: { name, hz } }) =>
     console.log(`${name} × ${Math.floor(hz).toLocaleString()} op/s`)
