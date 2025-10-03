12d88c92d259e9aa4f28059c63254294190e3907
Liam Tait <liam@runn.io>
2025-10-03 15:03:47 +1300

    Add node:util.styleText to benchmark

--- bench/index.js
+++ bench/index.js
@@ -6,17 +6,22 @@ import kleur from "kleur"
 import colors from "colors"
 import picocolors from "picocolors"
 import ansicolors from "ansi-colors"
+import { styleText } from "node:util"
 
 const test = (c) =>
   c.red(`${c.bold(`${c.cyan(`${c.yellow("yellow")}cyan`)}`)}red`)
 
+const testNode = () => 
+  styleText("red", `${styleText(["bold", "cyan"], `${styleText("yellow", "yellow")}cyan`)}red`)
+
 new bench.Suite()
-  .add("  chalk       ", () => test(chalk))
-  .add("  kleur       ", () => test(kleur))
-  .add("  colors      ", () => test(colors))
-  .add("  ansi-colors ", () => test(ansicolors))
-  .add("  picocolors  ", () => test(picocolors))
-  .add("+ colorette   ", () => test(colorette))
+  .add("  chalk               ", () => test(chalk))
+  .add("  kleur               ", () => test(kleur))
+  .add("  colors              ", () => test(colors))
+  .add("  ansi-colors         ", () => test(ansicolors))
+  .add("  picocolors          ", () => test(picocolors))
+  .add("  node:util.styleText ", () => testNode())
+  .add("+ colorette           ", () => test(colorette))
   .on("cycle", ({ target: { name, hz } }) =>
     console.log(name, `${Math.floor(hz).toLocaleString()} ops/sec`.padStart(18))
   )
